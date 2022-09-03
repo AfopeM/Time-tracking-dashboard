45907bf6bde9a70d0e743fc937d16bc56eae288f
Afope <afopematilukuro@gmail.com>
2022-09-03 12:51:58 -0400

    Updated - base.scss - card.scss - layout.scss - index.html
    Redesigned Mobile display
    Desktop display completed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
 "use strict";
+// How to Obtain the information in an external File.
+async function dayData() {
+  return (await fetch("./script/data.json")).json();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let data;
+  try {
+    data = await dayData();
+  } catch (e) {
+    console.log("error");
+  }
+});
